88f721c7ce423a3ac1785e85133c2cf220525618
ayoshittu <55026063+shitboi@users.noreply.github.com>
2025-12-13 03:09:15 -0500

    Create setup.py
    Runs various commands to setup server and run the app

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+
+# clone repo and move to dir
+subprocess.run('git clone https://github.com/shitboi/fastApi-app.git')
+
+os.chdir('~/fastApi-app')
+
+#install apt package
+subprocess.run('apt install python3-fastapi python3-pandas python3-uvicorn nginx -y', shell=True)
+
+#get server ipAddress
+com = subprocess.run('ip a |grep "global eth0" |head -n 1',
+                     shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if com.returncode==0:
+    ip = com.stdout.decode().split(' ')[5].split('/')[0]
+
+    # replace servername with serverIP address
+    replace_com = f"sed -i 's/serverPublicIP/{ip}/g' fastapi-nginx"
+    replacement = subprocess.run(replace_com, shell=True)
+    if replacement.returncode==0:
+        # copy file to nginx site-enabled dir and restart nginx
+        subprocess.run('cp fastapi-nginx /etc/nginx/site-enabled/ && service nginx restart', shell=True)
+
+        # start fastapi app
+        subprocess.run('python3 -m uvicorn main:app')
+
+
